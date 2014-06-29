f9c9bf0be572bbe25a35951f88d59ffa7aec15dc
John Flanagan <johnflan@gmail.com>
2014-06-29 17:50:18 +0100

    ball bounce behaviour

--- src/com/johnflan/Pong.java
+++ src/com/johnflan/Pong.java
@@ -14,6 +14,7 @@ public class Pong extends JPanel {
     public static final int PUCK_WIDTH = 100;
 
     public static final int BALL_SIZE = 15;
+    public static final int PIXEL_MOVE_PER_FRAME = 2;
 
     public boolean BALL_DIRECTION_DOWN = true;
     public boolean BALL_DIRECTION_RIGHT = true;
@@ -21,78 +22,11 @@ public class Pong extends JPanel {
     public int awayPenalty = 0;
     public int homePenalty = 0;
 
-    int x = 0;
-    int y = 0;
+    int ballX = 0;
+    int ballY = WINDOW_HEIGHT / 2;
 
-    private void moveBall() {
-
-        if (x + BALL_SIZE > WINDOW_WIDTH){
-            BALL_DIRECTION_RIGHT = false;
-        }
-
-        if (x < 0){
-            BALL_DIRECTION_RIGHT = true;
-        }
-
-        if (BALL_DIRECTION_RIGHT){
-            x = x + 1;
-        } else {
-            x = x - 1;
-        }
-
-
-        if(y > WINDOW_HEIGHT){
-            BALL_DIRECTION_DOWN = false;
-            homePenalty = 255;
-        }
-
-        if (y < 0){
-            BALL_DIRECTION_DOWN = true;
-            awayPenalty = 255;
-        }
-
-        if(BALL_DIRECTION_DOWN){
-            y = y + 1;
-        } else {
-            y = y - 1;
-        }
-
-        if (homePenalty - 2 > 0){
-            homePenalty -= 2;
-        }
-
-        if (awayPenalty - 2 > 0){
-            awayPenalty -= 2;
-        }
-    }
-
-    @Override
-    public void paint(Graphics g) {
-        super.paint(g);
-        Graphics2D g2d = (Graphics2D) g;
-        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-
-        //AWAY PENALTY
-        g2d.setColor(new Color(255, 0, 0, awayPenalty));
-        g2d.fillRect(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT / 2);
-
-        //HOME PENALTY
-        g2d.setColor(new Color(255, 0, 0, homePenalty));
-        g2d.fillRect(0, (WINDOW_HEIGHT / 2) + 1, WINDOW_WIDTH, (WINDOW_HEIGHT / 2) + 20);
-
-        g2d.setColor(Color.WHITE);
-
-        //AWAY PUCK
-        g2d.fillRect(x - (PUCK_WIDTH / 2), 15, PUCK_WIDTH, PUCK_HEIGHT);
-
-        //HOME PUCK
-        g2d.fillRect(MouseInfo.getPointerInfo().getLocation().x - (PUCK_WIDTH / 2), WINDOW_HEIGHT - 15, PUCK_WIDTH, PUCK_HEIGHT);
-
-        drawPitch(g);
-
-        //BALL
-        g2d.fillOval(x, y, BALL_SIZE, BALL_SIZE);
-    }
+    int homePuckX = 0;
+    int awayPuckX = 0;
 
     public static void main(String[] args) throws InterruptedException {
 
@@ -101,7 +35,7 @@ public class Pong extends JPanel {
         game.setBackground(Color.BLACK);
 
         frame.add(game);
-        frame.setBackground(Color.RED);
+        frame.setBackground(Color.BLACK);
         frame.setSize(WINDOW_WIDTH, WINDOW_HEIGHT + 45);
         frame.setVisible(true);
         frame.setLocationRelativeTo(null);
@@ -115,17 +49,132 @@ public class Pong extends JPanel {
         frame.setCursor(blankCursor);
 
         while (true) {
+
             game.moveBall();
             game.repaint();
-            Thread.sleep(5);
+            //15 ms is close to 60fps
+            Thread.sleep(10);
         }
     }
 
-    private void drawPitch(Graphics g) {
+    private void moveBall() {
 
-        int currentWidth = 10;
+        //Determine ball horizontal direction
+        if (ballX + BALL_SIZE > WINDOW_WIDTH){
+            BALL_DIRECTION_RIGHT = false;
+        }
+
+        if (ballX < 0){
+            BALL_DIRECTION_RIGHT = true;
+        }
+
+        if (BALL_DIRECTION_RIGHT){
+            ballX = ballX + PIXEL_MOVE_PER_FRAME;
+        } else {
+            ballX = ballX - PIXEL_MOVE_PER_FRAME;
+        }
+
+
+        //Determine away ball puck bounce
+        if(ballY == 28 && BALL_DIRECTION_DOWN == false && (awayPuckX - 50) < ballX  && (awayPuckX + 50) > ballX ){
+            BALL_DIRECTION_DOWN = !BALL_DIRECTION_DOWN;
+        }
+
+        //Determine home ball puck bounce
+        if(ballY == WINDOW_HEIGHT - 30 && BALL_DIRECTION_DOWN == true && (homePuckX - 50 - (PUCK_WIDTH / 2)) <= ballX  && (homePuckX + 50) > ballX ){
+            BALL_DIRECTION_DOWN = !BALL_DIRECTION_DOWN;
+        }
+
+        //Determine ball vertical direction
+        if(ballY > WINDOW_HEIGHT){
+            BALL_DIRECTION_DOWN = false;
+            homePenalty = 255;
+        }
+
+        if (ballY < 0){
+            BALL_DIRECTION_DOWN = true;
+            awayPenalty = 255;
+        }
+
+        if(BALL_DIRECTION_DOWN){
+            ballY = ballY + PIXEL_MOVE_PER_FRAME;
+        } else {
+            ballY = ballY - PIXEL_MOVE_PER_FRAME;
+        }
+
+        //Determine penalty
+        if (homePenalty - 5 > 0){
+            homePenalty -= 5;
+        }
+
+        if (awayPenalty - 5 > 0){
+            awayPenalty -= 5;
+        }
+
+        //update puck position
+        awayPuckX = ballX;
+        homePuckX = MouseInfo.getPointerInfo().getLocation().x;
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        drawFrame(g2d);
+    }
+
+    private void drawFrame(Graphics2D g2d) {
+
+        g2d.setColor(Color.WHITE);
+
+        //AWAY PUCK
+        g2d.fillRect(awayPuckX - (PUCK_WIDTH / 2), 15, PUCK_WIDTH, PUCK_HEIGHT);
+
+        //HOME PUCK
+        g2d.fillRect(homePuckX - (PUCK_WIDTH / 2), WINDOW_HEIGHT - 15, PUCK_WIDTH, PUCK_HEIGHT);
+
+        //BALL
+        g2d.fillOval(ballX, ballY, BALL_SIZE, BALL_SIZE);
+
+        drawInterlacePattern(g2d);
+        drawPitchHalfwayLine(g2d);
+
+        //AWAY PENALTY
+        if(awayPenalty > 0){
+            drawAwayPenalty(g2d);
+        }
+
+        //HOME PENALTY
+        if(homePenalty > 0){
+            drawHomePenalty(g2d);
+        }
+    }
+
+    private void drawHomePenalty(Graphics2D g2d) {
+        g2d.setColor(new Color(255, 0, 0, homePenalty));
+        g2d.fillRect(0, (WINDOW_HEIGHT / 2) + PIXEL_MOVE_PER_FRAME, WINDOW_WIDTH, (WINDOW_HEIGHT / 2) + 20);
+    }
+
+    private void drawAwayPenalty(Graphics2D g2d) {
+        g2d.setColor(new Color(255, 0, 0, awayPenalty));
+        g2d.fillRect(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT / 2);
+    }
+
+    private void drawInterlacePattern(Graphics2D g2d) {
+        int interlaceLines = 0;
+        g2d.setColor(new Color(0, 0, 0, 70));
+        while (interlaceLines < WINDOW_HEIGHT){
+            interlaceLines += 2;
+            g2d.fillRect(0, interlaceLines, WINDOW_WIDTH, 1);
+        }
+    }
+
+    private void drawPitchHalfwayLine(Graphics2D g2d) {
+        int currentWidth = 12;
+        g2d.setColor(Color.WHITE);
         while(WINDOW_WIDTH > currentWidth){
-            g.fillRect(currentWidth, WINDOW_HEIGHT / 2, 15, 1);
+            g2d.fillRect(currentWidth, WINDOW_HEIGHT / 2, 15, PIXEL_MOVE_PER_FRAME);
             currentWidth += 30;
         }
     }
